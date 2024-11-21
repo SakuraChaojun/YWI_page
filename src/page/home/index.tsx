2b72a8208a971fbf8e06492e5caf8a7e79f83181
sakurachaojun <sakurachaojun@gmail.com>
2024-11-21 11:45:57 -0500

    add_static_login

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,5 +1,20 @@
+import { Button } from "antd";
+
+function Egbutton(props:any){
+    const cssStyle = {padding:'10px 20px',borderRadius:'10px',backgroundColor:props.type==='primary'?'blue':''}
+    return <button style={cssStyle}>{props.children}</button>
+
+}
+
+
+
 function Home(){
-    return <div>Home component</div>
+    return <div>
+    <Button type='primary'>我是按钮</Button>
+    <Egbutton type=''> </Egbutton>
+     Home component
+    </div>
+
 }
 
 export default Home;
--- src/page/login/index.tsx
+++ src/page/login/index.tsx
@@ -1,5 +1,55 @@
-function Login(){
-    return <div>login component</div>
+import logo from "../../assets/logo.506e999.png";
+import bg from "../../assets/bg.jpeg";
+import lgbg from "../../assets/bg2.jpg";
+import type { FormProps } from "antd";
+import { Button, Checkbox, Form, Input } from "antd";
+import "./index.scss";
+function Login() {
+  return (
+    <div className="login" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="part">
+        <div className="title">
+          <div className="logo">
+            <img src={logo} width={100} />
+          </div>
+          <h3>移民资讯管理平台</h3>
+        </div>
+        <Form
+          
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            label={null}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item label={null}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
