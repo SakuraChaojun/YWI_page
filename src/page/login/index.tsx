c05e45257a3a2e7ee2047c7d9057733494bb7e3f
sakurachaojun <sakurachaojun@gmail.com>
2024-11-22 11:21:59 -0500

    add_auth_form_1

--- src/page/login/index.tsx
+++ src/page/login/index.tsx
@@ -1,10 +1,19 @@
 import logo from "../../assets/logo.506e999.png";
 import bg from "../../assets/bg.jpeg";
-import lgbg from "../../assets/bg2.jpg";
 import type { FormProps } from "antd";
 import { Button, Checkbox, Form, Input } from "antd";
+import { UserOutlined,LockOutlined } from '@ant-design/icons';
 import "./index.scss";
 function Login() {
+
+  const[form] = Form.useForm();
+
+  function handleLogin(){
+    form.validateFields().then((res)=>{
+      console.log(res)
+    }).catch((err)=>{});
+  }
+  
   return (
     <div className="login" style={{ backgroundImage: `url(${bg})` }}>
       <div className="part">
@@ -12,25 +21,30 @@ function Login() {
           <div className="logo">
             <img src={logo} width={100} />
           </div>
-          <h3>移民资讯管理平台</h3>
+          <h2>亿微移民资讯管理平台</h2>
         </div>
-        <Form
-          
+
+       
+        <Form form={form}
+        initialValues={{remember:true}}
+        autoComplete="on"
+        
         >
+           
           <Form.Item
-            label="Username"
+            
             name="username"
-            rules={[{ required: true, message: "Please input your username!" }]}
+            rules={[{ required: true, message: "用户名不能为空" }]}
           >
-            <Input />
+            <Input placeholder="请输入您的用户名" prefix={<UserOutlined/>}/>
           </Form.Item>
 
           <Form.Item
-            label="Password"
+           
             name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
+            rules={[{ required: true, message: "密码不能为空" }]}
           >
-            <Input.Password />
+            <Input.Password placeholder="请输入您的密码" prefix={<LockOutlined/>}/>
           </Form.Item>
 
           <Form.Item
@@ -41,9 +55,10 @@ function Login() {
             <Checkbox>Remember me</Checkbox>
           </Form.Item>
 
-          <Form.Item label={null}>
-            <Button type="primary" htmlType="submit">
-              Submit
+          <Form.Item >
+            <Button type="primary" htmlType="submit" style={{width:"100%"}}
+            onClick={handleLogin}>  
+              登录
             </Button>
           </Form.Item>
         </Form>
